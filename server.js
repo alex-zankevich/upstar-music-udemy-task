5aeda54944fd79f144717b020afd9f915bb07a6d
Aliaksandr Zankevich <aliaksandr.zankevich@gmail.com>
2017-01-14 19:31:46 +0300

    add heroce Procfile

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
-
-const webpackMiddleware = require('webpack-dev-middleware');
-const webpack = require('webpack');
-const webpackConfig = require('./webpack.config');
+const path = require('path');
 
 const app = express();
 
-app.use(webpackMiddleware(webpack(webpackConfig)));
+if (process.env.NODE_ENV !== 'production') {
+	const webpackMiddleware = require('webpack-dev-middleware');
+	const webpack = require('webpack');
+	const webpackConfig = require('./webpack.config');
 
-app.listen(3050, () => console.log('Listening'));
+	app.use(webpackMiddleware(webpack(webpackConfig)));
+} else {
+	app.use(express.static('dist'));
+	app.get('*', (rerq, res) => {
+		res.sendFile(path.join(__dirname, 'dist/index.html'));
+	});
+}
+
+app.listen(process.env.PORT || 3050, () => console.log('Listening'));
